64134784adeeb3e36d6cebd9ca1c3ebefaa987d5
Tanmay <112268448+tanmaygupta100@users.noreply.github.com>
2023-12-09 13:23:14 -0800

    Update nucleusMachineCode.py
    Reworked how the timer operates: Using the DP segment to count the seconds instead, and changing the intervals to 1 minute instead of 5.
    Also updated the pin locations based on rewiring.

--- nucleusMachineCode.py
+++ nucleusMachineCode.py
@@ -8,17 +8,20 @@
         # Rotating Segment Timer
 
 
-import pygame #not yet installed, for headphone jack
-from gpiozero import Button, LED, Buzzer
+import pygame
+from gpiozero import Button, LED
 from time import sleep
 
+# Initialize pygame mixer
+pygame.mixer.init()
 
 # Define GPIO pins for buttons and LED:
-timerButton = Button(17, bounce_time=0.5)
-timer_segments = [LED(22), LED(23), LED(24), LED(25), LED(8), LED(7), LED(12)] # F, A, B, C, D, E, G
-lightButton = Button(18, bounce_time=0.5)
-rgbLED = LED(21)
-musicButton = Button(27, bounce_time=0.5)
+timer_button = Button(5, bounce_time=0.5)
+timer_segments = [LED(12), LED(10), LED(22), LED(21), LED(23), LED(27), LED(16)]  # F, A, B, C, D, E, G
+dp_segment = LED(19)  # Decimal point segment
+light_button = Button(38, bounce_time=0.5)
+rgb_led = LED(40)
+music_button = Button(3, bounce_time=0.5)
 
 # Timer variables
 current_segment = 0
@@ -39,15 +42,16 @@ def play_sound():
     global sound_playing
     if not sound_playing:
         sound_playing = True
-        # Add code to play the current sound file
-        print(f"Playing: {sounds[current_sound]}")
+        pygame.mixer.music.load(sounds[current_sound])
+        pygame.mixer.music.play()
 
 # Function to stop the current sound
 def stop_sound():
     global sound_playing
-    sound_playing = False
-    # Add code to stop the current sound
-    print("Stopping current sound")
+    if sound_playing:
+        pygame.mixer.music.stop()
+        sound_playing = False
+        print("Stopping current sound")
 
 # Function to start the timer
 def start_timer():
@@ -55,8 +59,10 @@ def start_timer():
     timer_running = True
     while timer_running and current_segment < 7:
         timer_segments[current_segment].on()
+        dp_segment.toggle()  # Toggle decimal point segment
         sleep(1)  # Flashing interval
         timer_segments[current_segment].off()
+        dp_segment.toggle()  # Toggle decimal point segment
         sleep(1)
         current_segment += 1
     if current_segment == 7:
@@ -69,6 +75,7 @@ def reset_timer():
     global current_segment, timer_running
     for segment in timer_segments:
         segment.off()
+    dp_segment.off()  # Turn off decimal point segment
     current_segment = 0
     timer_running = False
 
@@ -77,22 +84,22 @@ def toggle_light():
     global light_on
     light_on = not light_on
     if light_on:
-        led.color = light_modes[light_mode_index]
+        rgb_led.color = light_modes[light_mode_index]
     else:
-        led.off()
+        rgb_led.off()
 
 # Function to change the LED light mode
 def change_light_mode():
     global light_mode_index
     light_mode_index = (light_mode_index + 1) % len(light_modes)
     if light_on:
-        led.color = light_modes[light_mode_index]
+        rgb_led.color = light_modes[light_mode_index]
 
 # Assign functions to button events
 timer_button.when_pressed = start_timer
 timer_button.when_released = reset_timer
-led_button.when_pressed = toggle_light
-led_button.when_held = change_light_mode
+light_button.when_pressed = toggle_light
+light_button.when_held = change_light_mode
 music_button.when_pressed = play_sound
 music_button.when_held = stop_sound
 
@@ -106,6 +113,7 @@ finally:
     # Cleanup GPIO on program exit
     for segment in timer_segments:
         segment.off()
-    led.off()
+    dp_segment.off()
+    rgb_led.off()
     # Add code to stop any playing sound
     print("Cleanup complete.")
